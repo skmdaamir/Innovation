202e33b0b03b440a3e71cb6fcfc713b9735c4f2c
skmdaamir <53207758+skmdaamir@users.noreply.github.com>
2019-10-11 23:56:22 +0530

    All files are uploaded

---
+++ footer.php
@@ -0,0 +1,45 @@
+<footer class="pt-5 pb-4 bg-light top-marg" id="contact">
+          <div class="container">
+            <div class="row">
+                <ul class="f-address">
+                  <li>
+                    <div class="row">
+                      <div class="col-1"><i class="fas fa-map-marker"></i></div>
+                      <div class="col-10">
+                        <h6 class="font-weight-bold mb-0">Address:</h6>
+                        <p class="para">Mahatma Gandhi Mission College<br> of Engineering and Technology, Kamothe</p>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="row">
+                      <div class="col-1"><i class="far fa-envelope"></i></div>
+                      <div class="col-10">
+                        <h6 class="font-weight-bold mb-0">Have any questions?</h6>
+                        <p class="para"><a href="#">Support@iac.com</a></p>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="row">
+                      <div class="col-1"><i class="fas fa-phone-volume"></i></div>
+                      <div class="col-10">
+                        <h6 class="font-weight-bold mb-0"></h6>
+                        <p class="para"><a href="#">+91-8655319821</a></p>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-lg-3 col-md-6 col-sm-6 mt-2 mb-4">
+                <h5 class="mb-4 font-weight-bold"> &nbsp &nbsp &nbsp  &nbsp CONNECT WITH US</h5>
+                <ul class="social-pet mt-4">
+                  <li><a href="https://www.facebook.com/" title="facebook"><i class="fab fa-facebook-f"></i></a></li>
+                  <li><a href="https://www.twitter.com" title="twitter"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="https://www.aboutme.google.com" title="google-plus"><i class="fab fa-google-plus-g"></i></a></li>
+                  <li><a href="https://www.instagram.com" title="instagram"><i class="fab fa-instagram"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </footer>
---
+++ student.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 01, 2019 at 07:39 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `formpage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL,
+  `firstname` text NOT NULL,
+  `lastname` text NOT NULL,
+  `email` text NOT NULL,
+  `phone` text NOT NULL,
+  `interest` text NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
